fccfc2504fbd6e82d6c4afceb6ad8aa898c24864
ROM4n2 <beianderen@gmail.com>
2026-08-10 20:25:34 +0800

    feat: wire up main entry with worker pool and HTTP server

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
-package agentapi
+package main
+
+import (
+	"agent-api/api"
+	"agent-api/store"
+	"agent-api/worker"
+	"net/http"
+)
+
+func main() {
+	s := store.NewStore()
+	p := worker.NewPool(3, s)
+	p.Start()
+	defer p.Stop()
+
+	h := api.NewHandler(s, p)
+	mux := h.Routes()
+	http.ListenAndServe(":8080", mux)
+}
